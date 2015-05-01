6a0d79050f262551b06575637c2c45d56371be01
cd.montanez10 <cd.montanez10@uniandes.edu.co>
2015-05-01 14:30:14 -0500

    Modificaciones del properties para agregar servicios

--- src/main/java/com/thinktanksoft/stampurestyle/service/ApplicationConfig.java
+++ src/main/java/com/thinktanksoft/stampurestyle/service/ApplicationConfig.java
@@ -5,20 +5,75 @@
  */
 package com.thinktanksoft.stampurestyle.service;
 
+import java.io.FileInputStream;
 import java.util.Set;
 import javax.ws.rs.core.Application;
-
+import java.util.Properties;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 /**
  *
  * @author yerson
  */
 @javax.ws.rs.ApplicationPath("webresources")
 public class ApplicationConfig extends Application {
+    
 
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new java.util.HashSet<>();
-        addRestResourceClasses(resources);
+        /*boolean rateDesigns=false;
+        Properties prop = new Properties();
+	
+        InputStream input = null;
+        
+	try {
+		input = this.getClass().getResourceAsStream("showServices.properties");
+		// load a properties file
+		prop.load(input);
+		// get the property value
+		rateDesigns = prop.getProperty("RateDesign").equals("true");
+		
+	} catch (IOException ex) {
+		ex.printStackTrace();
+	} finally {
+		if (input != null) {
+			try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+        //System.out.println(rateDesigns);
+        //El archivo de propiedades indica si habilitar el servicio
+       if (rateDesigns) {
+            resources.add(com.thinktanksoft.stampurestyle.service.RateDesignREST.class);
+        }*/
+        
+        resources.add(com.thinktanksoft.stampurestyle.service.CategoriaFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.ColorFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.CompraFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.DisenoFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.DisenosxproddisFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.EstilosFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.PerfilFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.PersonaFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.ProddisxcompraFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.ProductoFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.ProductocolorFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.ProductodisenadoFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.ProductotallaFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.TallaFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.TarjetacreditoFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.TipopagoFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.UsuarioFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.UsuarioxperfilFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.OfertaFacadeREST.class);
+
+        //addRestResourceClasses(resources);
         return resources;
     }
 
@@ -35,6 +90,7 @@ public class ApplicationConfig extends Application {
         resources.add(com.thinktanksoft.stampurestyle.service.DisenoFacadeREST.class);
         resources.add(com.thinktanksoft.stampurestyle.service.DisenosxproddisFacadeREST.class);
         resources.add(com.thinktanksoft.stampurestyle.service.EstilosFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.OfertaFacadeREST.class);
         resources.add(com.thinktanksoft.stampurestyle.service.PerfilFacadeREST.class);
         resources.add(com.thinktanksoft.stampurestyle.service.PersonaFacadeREST.class);
         resources.add(com.thinktanksoft.stampurestyle.service.ProddisxcompraFacadeREST.class);
@@ -42,6 +98,7 @@ public class ApplicationConfig extends Application {
         resources.add(com.thinktanksoft.stampurestyle.service.ProductocolorFacadeREST.class);
         resources.add(com.thinktanksoft.stampurestyle.service.ProductodisenadoFacadeREST.class);
         resources.add(com.thinktanksoft.stampurestyle.service.ProductotallaFacadeREST.class);
+        resources.add(com.thinktanksoft.stampurestyle.service.RateDesignREST.class);
         resources.add(com.thinktanksoft.stampurestyle.service.TallaFacadeREST.class);
         resources.add(com.thinktanksoft.stampurestyle.service.TarjetacreditoFacadeREST.class);
         resources.add(com.thinktanksoft.stampurestyle.service.TipopagoFacadeREST.class);
